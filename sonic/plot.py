3cfd3819ad1c1b6ab173bf916664beaefb4603f3
Huan Nguyen <famish99@gmail.com>
2013-02-01 11:40:53 -0600

    Additional files from restructuring

---
+++ sonic/plot.py
@@ -0,0 +1,25 @@
+"""
+Module to handle all the common plotting needs
+"""
+import sonic.utils
+
+
+class Plotter:
+    """
+    Class used to plot
+    """
+
+    def __init__(self):
+        pass
+
+    def plot_time(self, signal):
+        """
+        Plot a time domain signal
+        """
+        pass
+
+    def plot_fft(self, signal):
+        """
+        Plot fft graph of the input signal
+        """
+        pass
